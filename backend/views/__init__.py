e7f5bb45e55f6b0c791ee83642db229ef7abf47c
Laureline David <ltouroumov@gmail.com>
2017-04-27 16:34:04 +0200

    Prettyfied dashboard

--- backend/views/__init__.py
+++ backend/views/__init__.py
@@ -3,7 +3,7 @@ from django.http import Http404
 from django.http import HttpRequest
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
-from api.models import Client, Sync
+from api.models import Client, Sync, Game
 from backend.models import Endpoint
 
 
@@ -37,12 +37,17 @@ def logout_view(request):
 
 @login_required
 def dashboard(request):
+    from datetime import datetime, timedelta
+    last_24h = datetime.now() - timedelta(seconds=24 * 3600)
+
     client_count = Client.objects.count()
-    sync_count = Sync.objects.count()
+    sync_count = Sync.objects.filter(date__gte=last_24h).count()
     endpoint_count = Endpoint.objects.count()
+    game_count = Game.objects.count()
 
     return render(request, "backend/dashboard.html", {
         'client_count': client_count,
         'sync_count': sync_count,
         'endpoint_count': endpoint_count,
+        'game_count': game_count,
     })
